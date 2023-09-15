66245b1ab7b59fea1f0841fcd704425b611130ce
Manuel Alvarado <manualvaradoibarr@gmail.com>
2023-09-15 14:38:12 -0600

    Se termino el trabajo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,132 @@
 import styles from "./App.module.css";
 
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import { ReactComponent as Delete } from "./delete.svg";
 import { ReactComponent as Edit } from "./edit.svg";
 
 export default function App() {
   const [title, setTitle] = useState("Agregar Producto");
   const [isAdding, setIsAdding] = useState(true);
-  const [products,setProducts] = useState([
+  const [products,setProducts] = useState([]); 
+  const [productEditing,setProductEditing] = useState(null); 
 
-  ]); 
+  useEffect(()=>{
+    const controller = new AbortController()
+    
+    const fetchProducts = async ()=>{
+      try{
+        const response = await fetch('http://localhost:3000/products');
+        const data = await response.json();
+
+        setProducts(data);
+      }catch(error){
+        console.log(err);
+      }
+    }
+
+    fetchProducts();
+
+    return()=>{
+      controller.abort(); 
+    }
+  },[]);
+
+  const handleSubmit = async (event) =>{
+    event.preventDefault(); 
+
+    const formData = new FormData(event.target); 
+    const data = Object.fromEntries(formData); 
+
+    if(isAdding){
+      const productToAdd = {
+        id: `00${products.length+1}`,
+        name: data.product,
+        description: data.description
+      }
+      await fetch('http://localhost:3000/products',{
+        method: "POST",
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: JSON.stringify(productToAdd)
+      })
+
+      setProducts((prevProducts)=>[...prevProducts,productToAdd]);
+    }else{
+      await fetch(`http://localhost:3000/products/${productEditing.id}`,{
+        method: "PUT",
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: JSON.stringify({
+          id: productEditing.id,
+          name: data.product,
+          description: data.description
+        })
+      })
+
+    setProducts((prevProducts)=>prevProducts.map(product=>{
+      if(product.id===productEditing.id){
+        return{
+          ...product, 
+          name: data.product, 
+          description: data.description
+        }
+      }
+
+
+      return product;
+    }));
+    setIsAdding(true); 
+    setProductEditing(null);
+  }
+    event.target.reset(); 
+  }
+
+  const handleEdit = (product)=>{
+    setIsAdding(false); 
+    setProductEditing(product);
+  }
+
+  const handleDelete = async (product)=>{
+    await fetch(`http://localhost:3000/products/${product.id}`, {
+      method: "DELETE"
+    })
+
+    setProducts((prevProducts)=>prevProducts.filter(
+      p => p.id !== product.id
+    ))
+  }
 
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{title}</h1>
       <div className={styles.container}>
         <section className={styles.formContainer}>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleSubmit}>
             <h3 className={styles.formTitle}>Informacion del Producto</h3>
             
             <div className={styles.formContent}>
               <div className={styles.formGroup}>
                 <label className={styles.label} htmlFor="product">Producto</label>
-                <input className={styles.input} type="text" id="product" placeholder="Producto..." />
+                <input 
+                  className={styles.input} 
+                  type="text" 
+                  id="product" 
+                  name="product" 
+                  placeholder="Producto..." 
+                  defaultValue={isAdding?'':productEditing.name}
+                />
               </div>
               <div className={styles.formGroup}>
                 <label className={styles.label} htmlFor="description">Descripcion</label>
-                <textarea className={styles.input} id="description" placeholder="Descripcion..." />
+                <textarea 
+                  className={styles.input} 
+                  id="description" 
+                  name="description" 
+                  placeholder="Descripcion..."
+                  defaultValue={isAdding?'':productEditing.description} 
+                />
               </div>
 
               <button className={styles.button}>
@@ -56,12 +156,12 @@ export default function App() {
                   <td>{product.name}</td>
                   <td>{product.description}</td>
                   <td>
-                    <button className={styles.iconButton}>
+                    <button className={styles.iconButton} onClick={()=>handleEdit(product)}>
                       <Edit className={styles.actionEditIcon}/>
                     </button>
                   </td>
                   <td>
-                    <button className={styles.iconButton}>
+                    <button className={styles.iconButton} onClick={()=>handleDelete(product)}>
                       <Delete className={styles.actionDeleteIcon}/>
                     </button>
                   </td>
