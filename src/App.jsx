1ac85a267036e48bc3d57b31739ebbc74dbaabce
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-14 19:57:34 -0600

    Se termino el diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import styles from './App.module.css'
 
 import { useState } from 'react'
+import { ReactComponent as Delete } from './delete.svg'
+import { ReactComponent as Edit } from './edit.svg'
 
 export default function App() {
   const [title, setTitle] = useState('Agregar Producto')
   const [isAdding, setIsAdding] = useState(true)
+  const [products, setProducts] = useState([
+    
+  ])
 
   return (
     <main className={styles.main}>
@@ -15,26 +20,58 @@ export default function App() {
           <form className={styles.form}>
             <h3 className={styles.formTitle}>Información del Producto</h3>
 
-            <div>
-              <div>
-                <label htmlFor='product'>Producto</label>
-                <input type='text' id='product' placeholder='Producto...' />
+            <div className={styles.formContent}>
+              <div className={styles.formGroup}>
+                <label className={styles.label} htmlFor='product'>Producto</label>
+                <input className={styles.input} type='text' id='product' placeholder='Producto...' />
               </div>
 
-              <div>
-                <label htmlFor='description'>Descripción</label>
-                <textarea id='description' placeholder='Descripcion...' />
+              <div className={styles.formGroup}>
+                <label className={styles.label} htmlFor='description'>Descripción</label>
+                <textarea className={styles.input} id='description' placeholder='Descripcion...' />
               </div>
 
-              <button>
+              <button className={styles.button}>
                 {isAdding ? 'Agregar' : 'Editar'}
               </button>
             </div>
           </form>
         </section>
 
-        <section>
+        <section className={styles.tableContainer}>
           <h3>Productos</h3>
+
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Editar</th>
+                <th>Borrar</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {products.map(product => (
+                <tr key={product.id}>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.description}</td>
+                  <td>
+                    <button className={styles.iconButton}>
+                      <Edit className={styles.actionEditIcon} />
+                    </button>
+                  </td>
+                  <td>
+                    <button className={styles.iconButton}>
+                      <Delete className={styles.actionDeleteIcon} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </section>
       </div>
     </main>
