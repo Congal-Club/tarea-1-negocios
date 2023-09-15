d5e32b052c4a20a1143498d021a05196b97cdcde
Manuel Alvarado <manualvaradoibarr@gmail.com>
2023-09-14 19:57:44 -0600

    Se termino el diseno

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import styles from "./App.module.css";
 
 import { useState } from "react";
+import { ReactComponent as Delete } from "./delete.svg";
+import { ReactComponent as Edit } from "./edit.svg";
 
 export default function App() {
   const [title, setTitle] = useState("Agregar Producto");
   const [isAdding, setIsAdding] = useState(true);
+  const [products,setProducts] = useState([
+
+  ]); 
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{title}</h1>
@@ -12,22 +18,57 @@ export default function App() {
         <section className={styles.formContainer}>
           <form className={styles.form}>
             <h3 className={styles.formTitle}>Informacion del Producto</h3>
-            <div>
-            <div>
-              <label htmlFor="product">Producto</label>
-              <input type="text" id="product" placeholder="Producto..." />
-            </div>
-            <div>
-              <label htmlFor="description">Descripcion</label>
-              <textarea id="description" placeholder="Descripcion..." />
-            </div>
+            
+            <div className={styles.formContent}>
+              <div className={styles.formGroup}>
+                <label className={styles.label} htmlFor="product">Producto</label>
+                <input className={styles.input} type="text" id="product" placeholder="Producto..." />
+              </div>
+              <div className={styles.formGroup}>
+                <label className={styles.label} htmlFor="description">Descripcion</label>
+                <textarea className={styles.input} id="description" placeholder="Descripcion..." />
+              </div>
 
-            <button>{isAdding ? "Agregar" : "Editar"}</button>
+              <button className={styles.button}>
+                {isAdding ? "Agregar" : "Editar"}
+              </button>
             </div>
           </form>
         </section>
-        <section>
-            <h3>Productos</h3>
+        <section className={styles.tableContainer}>
+          <h3>Productos</h3>
+
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Editar</th>
+                <th>Borrar</th>  
+              </tr>   
+            </thead>
+
+            <tbody>
+              {products.map(product=>(
+                <tr key={product.id}>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.description}</td>
+                  <td>
+                    <button className={styles.iconButton}>
+                      <Edit className={styles.actionEditIcon}/>
+                    </button>
+                  </td>
+                  <td>
+                    <button className={styles.iconButton}>
+                      <Delete className={styles.actionDeleteIcon}/>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </section>
       </div>
     </main>
